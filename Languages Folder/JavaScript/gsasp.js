3fb842bd14d44fe63bea170b579bea847d372344
Tayyeb Chaudhary <chtayyeb250@gmail.com>
2020-12-27 14:09:22 +0500

    master lawyer

---
+++ Languages Folder/JavaScript/gsasp.js
@@ -0,0 +1,14 @@
+gsap.from("h1", {
+   opacity:0,
+   duration:1,
+   y:-50,
+   delay:4.5
+  });
+  gsap.from("p", {duration:1, opacity:0, y:40, delay:12.3});
+
+gsap.from("h5", {duration:1, opacity:0, x:60, delay: 6.2})
+gsap.from("img", {duration:1, opacity:0, z:10, delay:9.3})
+  var kursorx = new kursor({
+    type: 1,
+    color: 'rgba(200,100,0)'
+})
